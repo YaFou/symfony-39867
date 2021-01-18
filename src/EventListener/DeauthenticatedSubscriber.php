074fabb178cea0e35729a67c68a692e725719a64
YaFou <33806646+YaFou@users.noreply.github.com>
2021-01-18 12:32:46 +0100

    initial commit

---
+++ src/EventListener/DeauthenticatedSubscriber.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Http\Event\DeauthenticatedEvent;
+
+class DeauthenticatedSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var \Symfony\Component\HttpFoundation\Request|null
+     */
+    private $request;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->request = $requestStack->getCurrentRequest();
+    }
+
+    public function onDeauthenticated(DeauthenticatedEvent $event): void
+    {
+        $this->request->getSession()->getBag('flashes')->add('success', 'event triggered!');
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [DeauthenticatedEvent::class => 'onDeauthenticated'];
+    }
+}
